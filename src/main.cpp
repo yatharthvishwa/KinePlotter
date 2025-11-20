a025357311464755d28fcc953764e1f9aedf74aa
yatharthvishwa <yatharth4665@gmail.com>
2025-11-20 23:01:23 +0530

    added drawrectangle and draw circle methods

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,10 @@ void calculate_hypotenuse(){
   hypotenuse = sqrt(sq(px) + sq(py));
   // hypotenuse_angle = (asin(px / hypotenuse));
   hypotenuse_angle = atan2(px, py);
-  Serial.println(hypotenuse);
-  Serial.println(hypotenuse_angle * rad_to_deg);
+  // Serial.println(hypotenuse);
+  // Serial.println(hypotenuse_angle * rad_to_deg);
 }
+
 
 float calculate_elbow_motor_angle(){
   // elbow_thetha2 = PI - acos((sq(L_1) + sq(L_2) - sq(P)) / (2 * L_1 * L_2));
@@ -39,10 +40,96 @@ float calculate_elbow_motor_angle(){
 float calculate_shoulder_motor_angle(){
   // shoulder_thetha1 = atan(py / px) - acos((sq(L_1) + sq(P) - sq(L_2)) / (2 * L_1 * P));
   inner_angle = acos( (sq(hypotenuse) + sq(L_1) - sq(L_2)) / (2 * hypotenuse * L_1));
-  Serial.println(inner_angle * rad_to_deg);
+  // Serial.println(inner_angle * rad_to_deg);
   shoulder_motor_angle = hypotenuse_angle - inner_angle;
-  Serial.println(shoulder_motor_angle * rad_to_deg);
+  // Serial.println(shoulder_motor_angle * rad_to_deg);
   return shoulder_motor_angle * rad_to_deg;
+}
+
+void drawRectangle(float x1, float y1, float x2, float y2) {
+
+  py = y1;
+  px = x1;
+
+  delay(1000);
+
+  // Left edge
+  for (py = y1; py <= y2; py += 0.1) {
+    Serial.print(x1);
+    Serial.print(" ");
+    Serial.println(py);
+    calculate_hypotenuse();
+
+    float elbow_angle = abs(calculate_elbow_motor_angle());
+    float shoulder_angle = abs(90 - calculate_shoulder_motor_angle());
+    ELBOW.write(elbow_angle);
+    SHOULDER.write(shoulder_angle);
+
+    delay(100);
+  }
+
+  // Top edge
+  for (px = x1; px <= x2; px += 0.1) {
+    Serial.print(px);
+    Serial.print(" ");
+    Serial.println(y2);
+    calculate_hypotenuse();
+
+    float elbow_angle = abs(calculate_elbow_motor_angle());
+    float shoulder_angle = abs(90 - calculate_shoulder_motor_angle());
+    ELBOW.write(elbow_angle);
+    SHOULDER.write(shoulder_angle);
+    
+    delay(100);
+  }
+  
+  // Right edge
+  for (py = y2; py >= y1; py -= 0.1) {
+    Serial.print(x2);
+    Serial.print(" ");
+    Serial.println(py);
+    calculate_hypotenuse();
+
+    float elbow_angle = abs(calculate_elbow_motor_angle());
+    float shoulder_angle = abs(90 - calculate_shoulder_motor_angle());
+    ELBOW.write(elbow_angle);
+    SHOULDER.write(shoulder_angle);
+    delay(100);
+  }
+
+  // Bottom edge
+  for (px = x2; px >= x1; px -= 0.1) {
+    Serial.print(px);
+    Serial.print(" ");
+    Serial.println(y1);
+    calculate_hypotenuse();
+
+    float elbow_angle = abs(calculate_elbow_motor_angle());
+    float shoulder_angle = abs(90 - calculate_shoulder_motor_angle());
+    ELBOW.write(elbow_angle);
+    SHOULDER.write(shoulder_angle);
+    
+    delay(100);
+  }
+}
+
+void draw_circle(float center_x, float center_y, float radius) {
+  for (float angle = 0; angle <= PI; angle += 0.1) {
+    px = center_x + radius * cos(angle);
+    py = center_y + radius * sin(angle);
+    delay(1000);
+    Serial.print(px);
+    Serial.print(" ");
+    Serial.println(py);
+    calculate_hypotenuse();
+
+    float elbow_angle = abs(calculate_elbow_motor_angle());
+    float shoulder_angle = abs(90 - calculate_shoulder_motor_angle());
+    ELBOW.write(elbow_angle);
+    SHOULDER.write(shoulder_angle);
+
+    delay(100);
+  }
 }
 
 void setup() {
@@ -67,6 +154,10 @@ void loop() {
       Serial.read(); // - Buffer contains: `['\n']` , `while` loop reads and discards `'\n'`              
     }
 
+    // drawRectangle(-5, 7, 2, 15);
+
+    // draw_circle(0,0,10);
+
     calculate_hypotenuse();
 
     float elbow_angle = abs(calculate_elbow_motor_angle());
@@ -81,6 +172,8 @@ void loop() {
     SHOULDER.write(shoulder_angle);
     ELBOW.write(elbow_angle);
 
+
+    
   }
 }
 
